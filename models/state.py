ef5ff92db5df607bb4376df28346e9bbd28e51ff
Lynnmich <lynnenmichuki@gmail.com>
2023-04-18 09:59:42 -0700

    Airbnb

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __table_name = 'states'
+    name = Column(String(128), nullable=False)
+
+    if storage_type == 'db':
+        cities = relationship("City", backref="state")
+    else:
+        @property
+        def cities(self):
+            city.list = []
+            for city in storage.all(City).value():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
