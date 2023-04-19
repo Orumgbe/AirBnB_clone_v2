f2e126b31543acfb0975d35a327df2fea8476710
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-19 08:44:54 -0700

    city fix

--- models/city.py
+++ models/city.py
@@ -9,10 +9,14 @@ from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
     if getenv('HBNB_STORAGE_TYPE') == "db":
+        __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """Initializes city class"""
