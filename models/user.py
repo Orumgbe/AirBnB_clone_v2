b819919fa66a5e5cf3d293389baef98d1457302a
Lynnmich <lynnenmichuki@gmail.com>
2023-04-19 05:02:22 -0700

    AirBnb

---
+++ models/user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines a user class"""
+import models
+from modules.base_model import BaseModel, Base
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+Class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    __tablename__ = 'users'
+    if getenv('HBNB_STORAGE_TYPE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='user',
+                              cascade='all, delete-orphan')
+        reviews = relationship('review', backref='user',
+                               cascade='all, delete-orphan')
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes the user class"""
+        super().__init__(*args, **kwargs)
