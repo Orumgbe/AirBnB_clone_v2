6907683a0294841bf314415a353b1bd1004ffbf6
Lynnmich <lynnenmichuki@gmail.com>
2023-04-19 02:13:39 -0700

    AirBnB

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    name = ""
+    """
+    Amenity class
+    back_populates indicates thatthe relationship is bidirectional with the
+    amenities attribute in the place class serving as a backref
+    """
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenity = relationship("Place", secondary="place_amenity",
+                                 back_populates="amenities")
