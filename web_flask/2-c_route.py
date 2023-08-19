aeb07ff6ebc958ad18106c1d221c0c4e11e5dc06
Orumgbe <orumgbeugbekile@gmail.com>
2023-08-19 07:17:47 -0700

    C with input

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for hello route"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """The hello url route"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    """The url /hbnb route"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """The C route, displays C along with text"""
+    output = 'C ' + text.replace('_', ' ')
+    return output
+
+
+if __name__ == "__main__":
+    app.run()
